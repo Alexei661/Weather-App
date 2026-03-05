7e458ed319c6879a3c7a5b318951d25a7f2254c5
Alexei661 <alex.46262@gmail.com>
2026-03-05 18:22:18 +0200

    Added a separate function for printing the weather, I also tried the caching locally but erased it after because the main project is with redis

--- backend/app.py
+++ backend/app.py
@@ -2,28 +2,39 @@ import urllib.request
 import json
 import os
 from dotenv import load_dotenv
+import time
+from typing import Dict, Any
 load_dotenv()
 # Function to fetch weather data
-def fetch_weather(city, api_key):
+def fetch_weather_from_api(city: str, api_key: str):
     base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
     complete_url = f"{base_url}{city}?unitGroup=metric&contentType=json&key={api_key}"
+
     try:
         with urllib.request.urlopen(complete_url) as response:
             data = response.read()
-            # Parse JSON data
             json_data = json.loads(data)
-            print("Weather data for:", json_data['address'])
-            for day in json_data['days']:
-                print(f"Date: {day['datetime']}, Temperature: {day['temp']}°C, Conditions: {day['conditions']}")
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code}, {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
+            return json_data  # ← RETURNS here
+    except Exception as e:
+        print(f"API error: {e}")
+        return None
+
 
 # Replace with your API key
 api_key = os.getenv("Weather_API")
 # Specify the city
 city = 'Timisoara'
 
-# Call function to fetch weather data
-fetch_weather(city, api_key)
+
+def print_weather(json_data):
+    if not json_data:
+        print("No data")
+        return
+    print("Weather data for:", json_data['address'])
+    for day in json_data['days']:
+        print(f"Date: {day['datetime']}, Temperature: {day['temp']}°C, Conditions: {day['conditions']}")
+
+
+data1=fetch_weather_from_api(city, api_key)
+print_weather(data1)
+
